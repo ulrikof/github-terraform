bed566299b83c2defa34be0dc5a3418b6b5c38ba
ulrikof <ulrikof@stud.ntnu.no>
2024-09-19 16:13:31 +0200

    video-05-03

---
+++ backend/variables.tf
@@ -0,0 +1,23 @@
+variable "rg_backend_name" {
+  type = string
+}
+
+variable "rg_backend_location" {
+  type = string
+}
+
+variable "sa_backend_name" {
+  type = string
+}
+
+variable "sc_backend_name" {
+  type = string
+}
+
+variable "kv_backend_name" {
+  type = string
+}
+
+variable "sa_backend_accesskey_name" {
+  type = string
+}
