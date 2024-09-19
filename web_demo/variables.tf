dd2ec66ccada5e3d04721a5209e4edeb0669f263
ulrikof <ulrikof@stud.ntnu.no>
2024-09-19 17:38:42 +0200

    video-05-05/06

---
+++ web_demo/variables.tf
@@ -0,0 +1,19 @@
+variable "rg_name" {
+  type = string
+}
+
+variable "rg_location" {
+  type = string
+}
+
+variable "sa_name" {
+  type = string
+}
+
+variable "index_document" {
+  type = string
+}
+
+variable "source_content" {
+  type = string
+}
