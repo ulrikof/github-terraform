89d7704f648f0a70b430bf7c8ca646076f937bb8
ulrikof <ulrikof@stud.ntnu.no>
2024-09-28 11:19:44 +0200

    corrected name of storage account backend

--- staticwebsite/providers.tf
+++ staticwebsite/providers.tf
@@ -11,7 +11,7 @@ terraform {
 terraform {
   backend "azurerm" {
     resource_group_name  = "uo_backend_rg"
-    storage_account_name = "uo1backend1sa0oxn7wsp"
+    storage_account_name = "uo1backend1sa5hl8swws"
     container_name       = "uo-sc-backend"
     key                  = "staticwebsite.terraform.tfstate"
   }
