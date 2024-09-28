a7e1203d2aea412bb249c7a64813efee439785ea
ulrikof <ulrikof@stud.ntnu.no>
2024-09-28 11:12:33 +0200

    deploy web

---
+++ staticwebsite/main.tf
@@ -0,0 +1,45 @@
+locals {
+  workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+
+  rg_name    = terraform.workspace == "default" ? "${var.rg_name}" : "${var.rg_name}-${terraform.workspace}"
+  sa_name    = terraform.workspace == "default" ? "${var.sa_name}" : "${var.sa_name}-${terraform.workspace}"
+  web_suffix = "<h1>${terraform.workspace}</h1>"
+}
+
+resource "random_string" "random_string" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+# Create resource group
+resource "azurerm_resource_group" "uo_rg_web" {
+  name     = local.rg_name
+  location = var.location
+}
+
+# Create storage account
+resource "azurerm_storage_account" "uo_sa_web" {
+  name                     = "${lower(local.sa_name)}${random_string.random_string.result}"
+  resource_group_name      = azurerm_resource_group.uo_rg_web.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  static_website {
+    index_document = var.index_document
+  }
+}
+
+resource "azurerm_storage_blob" "uo_index_html" {
+  name                   = var.index_document
+  storage_account_name   = azurerm_storage_account.uo_sa_web.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = "${var.source_content}${local.web_suffix}"
+}
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.uo_sa_web.primary_web_endpoint
+}
---
+++ staticwebsite/variables.tf
@@ -0,0 +1,23 @@
+variable "rg_name" {
+  type    = string
+  default = "uo_rg_staticwebsite"
+}
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "sa_name" {
+  type    = string
+  default = "uo1sa"
+}
+
+variable "index_document" {
+  type    = string
+  default = "<h1>Web page created with Terraform</h1>"
+}
+
+variable "source_content" {
+  type    = string
+  default = "index.html"
+}
