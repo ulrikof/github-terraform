667c9b697bfaa5aa948bd43f40e6c4450c48e9bc
ulrikof <ulrikof@stud.ntnu.no>
2024-10-02 17:03:48 +0200

    removed admin_ssh_key

--- module07/demo-infra/modules/virtual_machine_module/main.tf
+++ module07/demo-infra/modules/virtual_machine_module/main.tf
@@ -34,13 +34,13 @@ resource "azurerm_linux_virtual_machine" "linux_vm_01" {
     azurerm_network_interface.NIC_01.id
   ]
 
-  admin_ssh_key {
-    username   = data.azurerm_key_vault_secret.username.value
+  # admin_ssh_key {
+    # username   = data.azurerm_key_vault_secret.username.value
    # public_key = file("~/.ssh/id_rsa.pub")
    # Placeholder to resolve validate on github, should be replaced with a key that can be stored as a secret,
    # but as this is not the porpouse of this module the placeholder wil work to avoid errors.
-   public_key = "placeholder!"
-  }
+    # public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1z8R4X8Q8s1kl4/bAfX4e/rR0Y6C7r5sby77dXZ/F8N3AQO+o6B9q5T6F4Zw2I2pOvXT1W/gOjW1y8u5HwYfeX1szjxkksxsxfJZ5fJZbB2AXWfF4GlYppvS7W1x+FUpyZqH4FbzyA0JbjdrUBc7XHoMFkaIoKy5ogT8EZk+ZoEBZLOOi2AV/test-key@example.com"
+  # }
 
   os_disk {
     caching              = "ReadWrite"
